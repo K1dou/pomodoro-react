eb77e0de846298e3435d5cc79ab2df374914558e
K1dou <hique1276@gmail.com>
2025-04-07 10:20:03 -0300

    theme light or dark fix on menu component

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,9 @@ import DefaultInput from "./components/DefaultInput";
 import { Logo } from "./components/Logo";
 import { Menu } from "./components/Menu";
 
+import { Footer } from "./components/Footer";
 import "./styles/global.css";
 import "./styles/theme.css";
-import { Footer } from "./components/Footer";
 
 export default function App() {
   return (
--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,20 +1,73 @@
-import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from "lucide-react";
+import {
+  HistoryIcon,
+  HouseIcon,
+  MoonIcon,
+  SettingsIcon,
+  SunIcon,
+} from "lucide-react";
 import styles from "./styles.module.css";
+import { useEffect, useState } from "react";
 
 export function Menu() {
+  const [theme, setTheme] = useState<"dark" | "light">(() => {
+    const storedTheme =
+      (localStorage.getItem("theme") as "dark" | "light") || "dark";
+    return storedTheme;
+  });
+
+  const nextThemeIcon = {
+    dark: <SunIcon></SunIcon>,
+    light: <MoonIcon></MoonIcon>,
+  };
+
+  function handleThemeChange(event: React.MouseEvent) {
+    event.preventDefault();
+
+    setTheme((prevTheme) => {
+      const newTheme = prevTheme === "dark" ? "light" : "dark";
+      return newTheme;
+    });
+  }
+
+  useEffect(() => {
+    document.documentElement.setAttribute("data-theme", theme);
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+
   return (
     <nav className={styles.menu}>
-      <a className={styles.menuLink} href="#">
+      <a
+        className={styles.menuLink}
+        href="#"
+        aria-label="Ir para a página inicial"
+        title="Ir para a página inicial"
+      >
         <HouseIcon></HouseIcon>
       </a>
-      <a className={styles.menuLink} href="#">
+      <a
+        className={styles.menuLink}
+        href="#"
+        aria-label="Ir para a página de histórico"
+        title="Ir para a página de histórico"
+      >
         <HistoryIcon></HistoryIcon>
       </a>
-      <a className={styles.menuLink} href="#">
+      <a
+        className={styles.menuLink}
+        href="#"
+        aria-label="Ir para a página de configurações"
+        title="Ir para a página de configurações"
+      >
         <SettingsIcon></SettingsIcon>
       </a>
-      <a className={styles.menuLink} href="#">
-        <SunIcon></SunIcon>
+      <a
+        className={styles.menuLink}
+        href="#"
+        aria-label="Tema claro"
+        title="Tema claro"
+        onClick={handleThemeChange}
+      >
+        {nextThemeIcon[theme]}
       </a>
     </nav>
   );
