777f9912b1b8ddef35e4b4ab69adb19d312c103c
K1dou <hique1276@gmail.com>
2025-04-07 22:25:45 -0300

    implement task creation logic with validation and state update; add utility functions for cycle management

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -5,14 +5,55 @@ import Cycles from "../Cycles";
 import DefaultButton from "../DefaultButton";
 import DefaultInput from "../DefaultInput";
 import { FormEvent, useState } from "react";
+import { TaskModel } from "../../models/TaskModel";
+import { useTaskContext } from "../../contexts/TaskContext/useTaskContext";
+import { getNextCycle } from "../../utils/getNextCycle";
+import { getNextCycleType } from "../../utils/getNextCycleType";
+import { formatSecondsToMinutes } from "../../utils/formatSecondsToMinutes";
 
 export function MainForm() {
+  const { state, setState } = useTaskContext();
   const [taskname, setTaskName] = useState("");
+
+  const nextClycle = getNextCycle(state.currentCycle);
+  const nextClycleType = getNextCycleType(nextClycle);
 
   function handleCreateNewTask(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
 
-    throw new Error("Function not implemented.");
+    if (!taskname) return;
+
+    const taskName = taskname.trim();
+
+    if (taskName.length < 3) {
+      alert("A task deve ter pelo menos 3 caracteres.");
+      return;
+    }
+
+    const newTask: TaskModel = {
+      id: Date.now().toString(),
+      name: taskName,
+      startDate: Date.now(),
+      complete: null,
+      interruptDate: null,
+      duration: state.config[nextClycleType],
+      type: nextClycleType,
+    };
+    const secondsRemaining = newTask.duration * 60;
+
+    setState((prev) => {
+      return {
+        ...prev,
+        config: {
+          ...prev.config,
+        },
+        activeTask: newTask,
+        currentCycle: nextClycle,
+        secondsRemaining,
+        formattedSecondsRemaining: formatSecondsToMinutes(secondsRemaining),
+        tasks: [...prev.tasks, newTask],
+      };
+    });
   }
 
   return (
