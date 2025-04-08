62ed5791bfc46b71e4dc743c7531c9735dcc8ee3
K1dou <hique1276@gmail.com>
2025-04-07 21:10:06 -0300

    implement task creation form with controlled input and submit handler

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -4,16 +4,27 @@ import { PlayCircleIcon } from "lucide-react";
 import Cycles from "../Cycles";
 import DefaultButton from "../DefaultButton";
 import DefaultInput from "../DefaultInput";
+import { FormEvent, useState } from "react";
 
 export function MainForm() {
+  const [taskname, setTaskName] = useState("");
+
+  function handleCreateNewTask(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    throw new Error("Function not implemented.");
+  }
+
   return (
-    <form className={styles.form}>
+    <form onSubmit={handleCreateNewTask} className={styles.form}>
       <div className={styles.formRow}>
         <DefaultInput
           id="meuInput"
           type="text"
           placeholder="Digite algo"
           labelText="Task"
+          value={taskname}
+          onChange={(e) => setTaskName(e.target.value)}
         ></DefaultInput>
       </div>
 
