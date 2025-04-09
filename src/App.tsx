89f3a3d967ef9a274226dbf6cb2699f292bac146
K1dou <hique1276@gmail.com>
2025-04-08 21:43:35 -0300

    feat: refactor routing; extract MainRouter component and implement ScrolltoTop functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,16 @@
 import { TaskContextProvider } from "./contexts/TaskContext/TaskContextProvider";
-import Home from "./pages/Home";
 
 import { MessagesContainer } from "./components/MessagesContainer";
+import { MainRouter } from "./routers/MainRouter";
 import "./styles/global.css";
 import "./styles/theme.css";
-import { BrowserRouter, Route } from "react-router";
-import { Routes } from "react-router";
-import { NotFound } from "./pages/NotFound";
-import { AboutPomodoro } from "./pages/AboutPomodoro";
 
 export default function App() {
   return (
     <TaskContextProvider>
 
       <MessagesContainer>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Home></Home>}></Route>
-            <Route path="/about-pomodoro" element={<AboutPomodoro></AboutPomodoro>}></Route>
-
-
-            <Route path="*" element={<NotFound></NotFound>}></Route>
-          </Routes>
-        </BrowserRouter>
+        <MainRouter></MainRouter>
       </MessagesContainer>
 
     </TaskContextProvider >
---
+++ src/routers/MainRouter/index.tsx
@@ -0,0 +1,32 @@
+import { BrowserRouter, Route, Routes, useLocation } from "react-router";
+import { AboutPomodoro } from "../../pages/AboutPomodoro";
+import Home from "../../pages/Home";
+import { NotFound } from "../../pages/NotFound";
+import { useEffect } from "react";
+
+function ScrolltoTop() {
+    const { pathname } = useLocation();
+
+    useEffect(() => {
+        window.scrollTo({ top: 0, behavior: "smooth" })
+            ;
+
+    }, [pathname])
+
+    return null;
+}
+
+export function MainRouter() {
+
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home></Home>}></Route>
+                <Route path="/about-pomodoro" element={<AboutPomodoro></AboutPomodoro>}></Route>
+
+                <Route path="*" element={<NotFound></NotFound>}></Route>
+            </Routes>
+            <ScrolltoTop></ScrolltoTop>
+        </BrowserRouter>
+    )
+}
